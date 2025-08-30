7cb08a31d3ddbc91bf1d040871a4cb0bf1d3aa1d
Leo-Aandrew <153619876+Leo-Aandrew@users.noreply.github.com>
2025-08-30 20:57:13 +0530

    Add files via upload

---
+++ ClassAndObject1/Main.java
@@ -0,0 +1,63 @@
+package ClassAndObject1;
+
+public class Main {
+
+    // Class =  blueprint for creating an object.
+    // Object = instance of a class.
+    // Object = An entity that holds data (attributes) and can perform actions (methods)
+    // Object = reference data type.
+
+    // Constructor = special method to initialize object variables.
+    // Constructor = called automatically when an object is created.
+
+    //Overloaded constructors = class have multiple constructors with different parameters.
+
+    public static void main(String[] args){
+//---------------------------------------------------------------------------------------------------------------
+// OVERLOADED CONSTRUCTORS:
+//        StudentClass student = new StudentClass();
+//        StudentClass student1 = new StudentClass("zoro");
+//        StudentClass student2 = new StudentClass("luffy","gear5@gmail.com");
+//        StudentClass student3 = new StudentClass("leo","leo@gmail.com", 21);
+//
+//        System.out.println(student.name);
+//        System.out.println(student.email);
+//        System.out.println(student.age);
+//
+//        System.out.println(student1.name);
+//        System.out.println(student1.email);
+//        System.out.println(student1.age);
+//
+//        System.out.println(student2.name);
+//        System.out.println(student2.email);
+//        System.out.println(student2.age);
+//
+//        System.out.println(student3.name);
+//        System.out.println(student3.email);
+//        System.out.println(student3.age);
+//---------------------------------------------------------------------------------------------------------------
+// CONSTRUCTOR:
+//        StudentClass student1 = new StudentClass("leo", 21, 8.5);
+//        StudentClass student2 = new StudentClass("zoro", 22, 7.5);
+//        StudentClass student3 = new StudentClass("luffy", 19, 6.9);
+//
+//        student1.study();
+//        student2.study();
+//        student3.study();
+
+//---------------------------------------------------------------------------------------------------------------------
+// SIMPLE EXAMPLE:
+//        StudentClass c = new StudentClass();
+//
+//        // Attributes
+//        System.out.println(c.name);
+//        System.out.println(c.age);
+//        System.out.println(c.mark);
+//        System.out.println(c.gender);
+//        System.out.println(c.isWorking);
+//
+//        // Method
+//        c.message();
+//----------------------------------------------------------------------------------------------------------------------
+    }
+}
---
+++ ClassAndObject1/StudentClass.java
@@ -0,0 +1,63 @@
+package ClassAndObject1;
+
+public class StudentClass {
+
+//--------------------------------------------------------------------------------------------------------------
+// OVERLOADED CONSTRUCTORS:
+//    String name;
+//    String email;
+//    int age;
+//
+//    StudentClass(){
+//        this.name = "Guest";
+//        this.email = "not provided";
+//        this.age = 0;
+//    }
+//
+//    StudentClass(String name){
+//        this.name = name;
+//        this.email = "not provided";
+//        this.age = 0;
+//    }
+//
+//    StudentClass(String name, String email){
+//        this.name = name;
+//        this.email = email;
+//        this.age = 0;
+//    }
+//
+//    StudentClass(String name, String email, int age){
+//        this.name = name;
+//        this.email = email;
+//        this.age = age;
+//    }
+//---------------------------------------------------------------------------------------------------------------
+// CONSTRUCTOR:
+//    String name;
+//    int age;
+//    double gpa;
+//
+//    StudentClass(String name, int age, double gpa){
+//        this.name = name;
+//        this.age = age;
+//        this.gpa = gpa;
+//    }
+//
+//    void study(){
+//        System.out.println(this.name + " is studying");
+//    }
+//---------------------------------------------------------------------------------------------------------------
+// SIMPLE EXAMPLE : (ATTRIBUTES & METHODS)
+//    // Attributes
+//    String name = "leo";
+//    int age = 21;
+//    double mark = 95.8;
+//    char gender = 'M';
+//    boolean isWorking = true;
+//
+//    // Method
+//    void message(){
+//        System.out.printf("I am %s and I am %d years old.", name , age);
+//    }
+//----------------------------------------------------------------------------------------------------------------------
+}
