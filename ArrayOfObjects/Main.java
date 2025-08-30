93ae3952e6df2874db773649e0cbae1bb5b2c71f
Leo-Aandrew <153619876+Leo-Aandrew@users.noreply.github.com>
2025-08-30 20:58:35 +0530

    Add files via upload

---
+++ ArrayOfObjects/Main.java
@@ -0,0 +1,28 @@
+package ArrayOfObjects;
+
+public class Main {
+    public static void main(String[] args){
+// ARRAY OF OBJECTS (using Anonymous object):-
+//        Car[] cars = {
+//                new Car("Tata", "Red"),
+//                new Car("Audi", "Black"),
+//                new Car("Swift", "Blue")
+//               };
+//
+//        for(Car car : cars){
+//            car.drive();
+//        }
+//--------------------------------------------------------------------------------------------------------
+// ARRAY OF OBJECTS:-
+//        Car car1 = new Car("Tata", "Red");
+//        Car car2 = new Car("Audi", "Black");
+//        Car car3 = new Car("Swift", "Blue");
+//
+//        Car[] cars = {car1, car2, car3}; //array of objects
+//
+//        for(Car car : cars){
+//            car.drive();
+//        }
+//---------------------------------------------------------------------------------------------------------
+    }
+}
